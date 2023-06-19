16533217dc329674f5b72cad3c602fb68a9ed149
David Johnson <dmjohnson844@gmail.com>
2023-06-19 14:39:15 -0400

    continued develoment footer before styling

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,41 @@ class Footer extends React.Component {
         return(
             <>
             <img className="footer-img" src={footerImg} />
+
+                <div className="footer-section">
+                    <div className="footer-link-section">
+
+                        <div className="footer-link-container">
+                            <h4> SUPPORT</h4>
+                            <ul>
+                                <li>Submit a car</li>
+                                <li>Contact Us</li>
+                                <li>FAQ</li>
+                                <li>Stickers</li>
+                            </ul>
+                        </div>
+                        <div className="footer-link-container">
+                            <h4> ABOUT</h4>
+                            <ul>
+                                <li>Our Story</li>
+                                <li>Privacy</li>
+                                <li>Terms</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div className="subscription-container">
+                        <h6> Subscribe</h6>
+                        <p className="subscribe-p"> Be the first to know of the latest driver-focused cars for sale.</p>
+                            <form>
+                            <input type="email" placeholder="Enter email" name="email"></input>
+                            <button className="subscribeBtn"> SIGN UP</button>
+                            </form>
+                    </div>
+                </div>
             </>
+
+
 
         )
     }
